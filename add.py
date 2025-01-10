a9d76f227484c6224f4db7872374d102d67cf61c
sree <gudurivenusree@gmail.com>
2025-01-10 17:01:52 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,10 @@
+#This program adds 2 numbers
+
+num1=1.5
+num2=6.3
+
+#Add 2 numbers
+sum=num1+num2
+
+print()'The sum of {}[D0}[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C and {}[D1} is {2}' .format(num1, num2, sum)
+
